9e8d755326aba2a190512ebdfed4a87fcd0a1ed0
erwan <erwan-14730@outlook.fr>
2024-02-10 13:54:06 +0100

    user model, user controller, auth middleware & user routes

---
+++ backend/app.js
@@ -0,0 +1,37 @@
+const express = require("express");
+const mongoose = require("mongoose");
+const bodyParser = require("body-parser");
+const path = require("path");
+
+mongoose
+  .connect(
+    "mongodb+srv://erwanmdev:BtTxPjHYJOXp39UI@cluster0.1ssrgyv.mongodb.net/?retryWrites=true&w=majority",
+    { useNewUrlParser: true, useUnifiedTopology: true }
+  )
+  .then(() => console.log("Connexion à MongoDB réussie !"))
+  .catch(() => console.log("Connexion à MongoDB échouée !"));
+
+const app = express();
+// intercepte les requete qui ont un content type json
+
+app.use((req, res, next) => {
+  //  permet d'accéder à notre API depuis n'importe quelle origine ( '*' ) ;
+  res.setHeader("Access-Control-Allow-Origin", "*");
+  // ajouter les headers mentionnés aux requêtes envoyées vers notre API (Origin , X-Requested-With , etc.) ;
+  res.setHeader(
+    "Access-Control-Allow-Headers",
+    "Origin, X-Requested-With, Content, Accept, Content-Type, Authorization"
+  );
+  // d'envoyer des requêtes avec les méthodes mentionnées ( GET ,POST , etc.).
+  res.setHeader(
+    "Access-Control-Allow-Methods",
+    "GET, POST, PUT, DELETE, PATCH, OPTIONS"
+  );
+  next();
+});
+
+app.use(bodyParser.json());
+
+// routes here
+
+module.exports = app;
